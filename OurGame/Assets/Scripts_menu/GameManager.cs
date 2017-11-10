23960764720e3c8402e4ffec98ddf8a6d77256e2
Ariane Leboeuf-Gagnon <aleboeuf-gagnon@acquisio.com>
2017-11-10 16:20:32 -0500

    Working main menu (only main menu)

---
+++ OurGame/Assets/Scripts_menu/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public UIManager ui;
+    public Canvas canvasMenu;
+    public Canvas canvasNew;
+    public Canvas canvasLoad;
+    public Canvas canvasOption;
+
+    // Use this for initialization
+    void Start () {
+        canvasMenu = GameObject.Find("Canvas_Main_Menu").GetComponent<Canvas>();
+        canvasNew = GameObject.Find("Canvas_New_Game").GetComponent<Canvas>();
+        canvasLoad = GameObject.Find("Canvas_Load_Game").GetComponent<Canvas>();
+        canvasOption = GameObject.Find("Canvas_Option").GetComponent<Canvas>();
+        ui = GetComponent<UIManager>();
+
+        DisableCanvas();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        canvasMenu.enabled = false;
+        canvasNew.enabled = true;
+    }
+
+    public void LoadGame()
+    {
+        canvasLoad.enabled = true;
+        canvasMenu.enabled = false;
+
+    }
+
+    public void DeleteGame()
+    {
+        canvasLoad.enabled = true;
+        canvasMenu.enabled = false;
+
+    }
+
+    public void Option()
+    {
+        canvasOption.enabled = true;
+        canvasMenu.enabled = false;
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void DisableCanvas()
+    {
+        canvasNew.enabled = false;
+        canvasLoad.enabled = false;
+        canvasOption.enabled = false;
+    }
+}
